779cde5199d8dadcdb4806da4b2e96b1254766b1
poojakakuru <poojareddykakuru@gmail.com>
2020-10-14 10:32:02 +0530

    6 digit number validatiom

--- pinCode.sh
+++ pinCode.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "Welcome to Postal pincode program"
+
+read -p "Enter Pincode:" pincode
+
+pincodepattern="[0-9]{6}"
+
+if [[ $pincode =~ $pincodepattern ]]
+then
+   echo "valid"
+else
+   echo "Invalid"
+fi
